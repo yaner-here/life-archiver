8a0c8453efcbcfcc6183a1b2a4957327e5619ca1
Yaner <26623948+yaner-here@users.noreply.github.com>
2025-11-22 19:29:15 +0800

    style(layout): use LightRays components as background in global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
 
+import LightRays from "@/components/LightRays";
+
 const schibstedGrotesk = Schibsted_Grotesk({
 	variable: "--font-schibsted-grotesk",
 	subsets: ["latin"],
@@ -28,7 +30,22 @@ export default function RootLayout({
 				<link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
 			</head>
 			<body className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}>
-				{children}
+				<div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+					<LightRays
+						raysOrigin="top-center-offset"
+						raysColor="#5dfeca"
+						raysSpeed={0.5}
+						lightSpread={0.9}
+						rayLength={1.4}
+						followMouse={true}
+						mouseInfluence={0.02}
+						noiseAmount={0.0}
+						distortion={0.01}
+					/>
+				</div>
+				<main>
+					{children}
+				</main>
 			</body>
 		</html>
 	);
